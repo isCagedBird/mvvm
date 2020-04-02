008bcf4652d141ab1148fbfe331dd47b259d31a1
isCagedBird <vogel_im_kafig2016@outlook.com>
2020-04-02 18:18:34 +0800

    add ajax function

--- js/mvvm.js
+++ js/mvvm.js
@@ -1,11 +1,11 @@
-function MVVM(options) {
+function MVVM (options) {
     this.$options = options || {};
     var data = this._data = this.$options.data;
     var me = this;
 
     // 数据代理
     // 实现 vm.xxx -> vm._data.xxx
-    Object.keys(data).forEach(function(key) {
+    Object.keys(data).forEach(function (key) {
         me._proxyData(key);
     });
 
@@ -18,37 +18,50 @@ function MVVM(options) {
 
 MVVM.prototype = {
     constructor: MVVM,
-    $watch: function(key, cb, options) {
+    $watch: function (key, cb, options) {
         new Watcher(this, key, cb);
     },
 
-    _proxyData: function(key, setter, getter) {
+    _proxyData: function (key, setter, getter) {
         var me = this;
-        setter = setter || 
-        Object.defineProperty(me, key, {
-            configurable: false,
-            enumerable: true,
-            get: function proxyGetter() {
-                return me._data[key];
-            },
-            set: function proxySetter(newVal) {
-                me._data[key] = newVal;
-            }
-        });
+        setter = setter ||
+            Object.defineProperty(me, key, {
+                configurable: false,
+                enumerable: true,
+                get: function proxyGetter () {
+                    return me._data[key];
+                },
+                set: function proxySetter (newVal) {
+                    me._data[key] = newVal;
+                }
+            });
     },
 
-    _initComputed: function() {
+    _initComputed: function () {
         var me = this;
         var computed = this.$options.computed;
         if (typeof computed === 'object') {
-            Object.keys(computed).forEach(function(key) {
+            Object.keys(computed).forEach(function (key) {
                 Object.defineProperty(me, key, {
-                    get: typeof computed[key] === 'function' 
-                            ? computed[key] 
-                            : computed[key].get,
-                    set: function() {}
+                    get: typeof computed[key] === 'function'
+                        ? computed[key]
+                        : computed[key].get,
+                    set: function () { }
                 });
             });
         }
     }
 };
+
+const func = (url, methods = 'get', data = null) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.open(methods, url)
+        xhr.onreadystatechange = () => {
+            xhr.readyState === 4 && (
+                xhr.status === 200 ? resolve(JSON.parse(xhr.responseText)) : reject(`error: ${xhr.status}`)
+            )
+        }
+        xhr.send(methods === 'get' ? null : JSON.stringify(data))
+    })
+}
